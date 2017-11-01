f4d30b96a79f8051e8fa031ed4af84172b8e6e6f
nosorrow <plamenorama@gmail.com>
2017-11-01 18:03:49 +0200

    Restaurant-demo

--- template-index.php
+++ template-index.php
@@ -7,16 +7,12 @@
     <link rel="shortcut icon" href="favicon.ico">
     <link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin,cyrillic-ext' rel='stylesheet'
           type='text/css'>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
-    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
-    <link rel="stylesheet" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
-    <script src="https://cdn.jsdelivr.net/scrollreveal.js/3.1.4/scrollreveal.min.js"></script>
-    <script src="https://cdn.rawgit.com/imsky/holder/master/holder.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css"
+          href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
 <div class="facebook-left">
@@ -46,44 +42,45 @@
         </div>
     </div>
 </nav>
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
     <!-- Indicators -->
     <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1"></li>
         <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
 
+    </ol>
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
         <div class="item active">
             <img src="img/oktopod.jpg" alt="Restaurant" class="img-responsive">
             <div class="carousel-caption">
-                <h1>Taste of the East</h1>
+                <h1 style="color: yellow;">Taste of the East</h1>
                 <h3>I'm a paragraph. here add your own text and edit me.</h3>
                 <p><a class="btn btn-lg btn-warning btn-resized" href="#" role="button">поръчай</a></p>
             </div>
         </div>
-
         <div class="item">
-            <img src="img/furna.jpg" alt="Furna"  class="img-responsive">
+            <img src="img/furna.jpg" alt="Furna" class="img-responsive">
             <div class="carousel-caption">
                 <h1>Taste of the East</h1>
                 <h3>I'm a paragraph. here add your own text and edit me.</h3>
                 <p><a class="btn btn-lg btn-info btn-resized" href="#" role="button">поръчай</a></p>
             </div>
         </div>
-
         <div class="item">
-            <img src="img/shots.jpg" alt="Shots"  class="img-responsive">
+            <img src="img/shots.jpg" alt="Shots" class="img-responsive">
             <div class="carousel-caption">
                 <h1>Taste of the East</h1>
                 <h3>I'm a paragraph. here add your own text and edit me.</h3>
                 <p><a class="btn btn-lg btn-success btn-resized" href="#" role="button">поръчай</a></p>
             </div>
         </div>
+        <div class="item">
+            <img src="img/chefRest.jpg" alt="Shots" class="img-responsive">
+        </div>
     </div>
-
     <!-- Left and right controls -->
     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
@@ -94,7 +91,6 @@
         <span class="sr-only">Next</span>
     </a>
 </div>
-
 <!-- ------------------- Add (About container) --------------------------------- -->
 <div id="about" class="container container-padding">
     <div class="row text-center">
@@ -132,7 +128,7 @@
             <div class="text-menu">
                 <div class="row slideanim">
                     <div class="col-sm-9">
-                        <h4>Телешки домашен бургер 'The Cef' </h4>
+                        <h4>Телешки домашен бургер 'The Chef'</h4>
                         <p>Assortment of fresh baked fruit breads and muffins</p>
                     </div>
                     <div class="col-sm-3">
@@ -190,14 +186,12 @@
     </div>
     <div class="row">
         <div class="col-md-12 text-center">
-            <a href="#" class="full-menu-link">цялото меню <span class="caret"></span></a>
+            <a href="#" class="btn btn-lg btn-black">цялото меню</a>
         </div>
     </div>
 </div>
 <hr>
-
 <!-- ------------------- ADD Chef Container -------------->
-
 <div id="chef" class="container container-padding">
     <div class="row text-center">
         <h4 class="section-title">Главния готвач</h4>
@@ -330,12 +324,14 @@
         </div>
     </div>
 </div>
-<!-- -------------- Template -  JavaScript ------------------------------------------- -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+<script src="https://cdn.jsdelivr.net/scrollreveal.js/3.1.4/scrollreveal.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <script src="js/template-js.js"></script>
-<script src="js/js.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
 <script>
-    window.addEventListener("load", function(){
+    window.addEventListener("load", function () {
         window.cookieconsent.initialise({
             "palette": {
                 "popup": {
@@ -352,7 +348,8 @@
                 "link": "Научи повече",
                 "href": "https://www.google.com/intl/bg/policies/technologies/cookies/"
             }
-        })});
+        })
+    });
 </script>
 </body>
 </html>
